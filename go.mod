97b250914b70712e829d430871be50cdcc2e5f54
Zill_Laiss <zill_laiss@proton.me>
2025-12-11 13:44:22 +0700

    finish migration to vanilla CSS & update to fest v0.8.0

--- go.mod
+++ go.mod
@@ -4,16 +4,16 @@ go 1.24.4
 
 require (
 	github.com/yuin/goldmark v1.7.13
-	go.abhg.dev/goldmark/frontmatter v0.2.0 // indirect
+	go.abhg.dev/goldmark/frontmatter v0.3.0 // indirect
 )
 
 require (
-	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/BurntSushi/toml v1.5.0 // indirect
 	go.abhg.dev/goldmark/toc v0.12.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
-	github.com/a-h/templ v0.3.920
-	github.com/zilllaiss/fest v0.7.0
+	github.com/a-h/templ v0.3.960
+	github.com/zilllaiss/fest v0.8.0
 )
--- routes.go
+++ routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+
 	"xyago/types"
 	"xyago/views"
 
@@ -12,31 +13,39 @@ import (
 	"github.com/a-h/templ"
 )
 
+const postStyling = "/assets/styles/post.css"
+
 func generator() *fest.Generator {
-	g := fest.NewGenerator(context.Background(), "Zill_Laiss' blog", nil)
+	g := fest.NewGenerator(context.Background(), "Zill_Laiss' blog", &fest.GeneratorConfig{
+		BaseConfig: temfest.BaseConfig{Lang: "en"},
+	})
 
 	g.CopyDir("assets", "")
 	g.CopyFile("favicon.png", "")
 	g.CopyFile("misc/README.md", "")
 
-	g.Head.Add(
+	g.HeadBody.Head(
 		temfest.ImportStyle("/assets/styles/global.css"),
 		temfest.ImportIcon("/favicon.png", "image/png"),
 	)
 
-	g.AddRouteFunc("/", index)
+	g.AddRouteFunc("/", index).HeadBody.Head(temfest.ImportStyle(postStyling))
 	g.AddRoute("/404.html", views.NotFound())
 	g.AddRoute("/about", views.About()).SetTitle("About")
-	g.AddRoute("/tags", views.Tags(tagsSorted))
+	g.AddRoute("/tags", views.Tags(tagsSorted)).SetTitle("Tags")
 
-	fest.NewRoutes("/tags/{s}", tagsSorted).
-		SetTitle("{s}").AddToGenerator(g, tagsFn)
+	tagRoutes := fest.NewRoutes("/tags/{s}", tagsSorted)
+	tagRoutes.HeadBody.Head(temfest.ImportStyle(postStyling))
+	tagRoutes.AddToGenerator(g, tagsFn)
 
-	fest.NewRoutesT("/posts/{s}", posts).
-		SetTitle("{s}").AddToGenerator(g, postsFn)
+	postRoutes := fest.NewRoutesT("/posts/{s}", posts)
+	postRoutes.HeadBody.Head(temfest.ImportStyle(postStyling))
+	postRoutes.AddToGenerator(g, postsFn)
 
-	fest.NewPaginatedRoutes("/blog/{s}", posts, 5).
-		SetTitle("Blogs - page {s}").AddToGenerator(g, blogs)
+	blogPages := fest.NewPaginatedRoutes("/blog/{s}", posts, 5).
+		SetTitle("Blogs - page {s}")
+	blogPages.HeadBody.Head(temfest.ImportStyle(postStyling))
+	blogPages.AddToGenerator(g, blogs)
 
 	return g
 }
